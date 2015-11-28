513d0ec96e470fe2778fe08251e7e9ac460892ad
Matt Walker <walkermatt@longwayaround.org.uk>
2015-11-28 11:32:24 +0000

    Use indexedMap in ex3 to remove the need to manually manage counter IDs

--- examples/3/CounterList.elm
+++ examples/3/CounterList.elm
@@ -8,19 +8,13 @@ import Html.Events exposing (..)
 
 -- MODEL
 
-type alias Model =
-    { counters : List ( ID, Counter.Model )
-    , nextID : ID
-    }
+type alias Model = { counters : List Counter.Model }
 
 type alias ID = Int
 
 
 init : Model
-init =
-    { counters = []
-    , nextID = 0
-    }
+init = { counters = [] }
 
 
 -- UPDATE
@@ -35,38 +29,37 @@ update : Action -> Model -> Model
 update action model =
   case action of
     Insert ->
-      let newCounter = ( model.nextID, Counter.init 0 )
+      let newCounter =  Counter.init 0
           newCounters = model.counters ++ [ newCounter ]
       in
           { model |
-              counters = newCounters,
-              nextID = model.nextID + 1
+              counters = newCounters
           }
 
     Remove ->
       { model | counters = List.drop 1 model.counters }
 
     Modify id counterAction ->
-      let updateCounter (counterID, counterModel) =
+      let updateCounter counterID counterModel =
               if counterID == id then
-                  (counterID, Counter.update counterAction counterModel)
+                  Counter.update counterAction counterModel
               else
-                  (counterID, counterModel)
+                  counterModel
       in
-          { model | counters = List.map updateCounter model.counters }
+          { model | counters = List.indexedMap updateCounter model.counters }
 
 
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  let counters = List.map (viewCounter address) model.counters
+  let counters = List.indexedMap (viewCounter address) model.counters
       remove = button [ onClick address Remove ] [ text "Remove" ]
       insert = button [ onClick address Insert ] [ text "Add" ]
   in
       div [] ([remove, insert] ++ counters)
 
 
-viewCounter : Signal.Address Action -> (ID, Counter.Model) -> Html
-viewCounter address (id, model) =
+viewCounter : Signal.Address Action -> ID -> Counter.Model -> Html
+viewCounter address id model =
   Counter.view (Signal.forwardTo address (Modify id)) model
