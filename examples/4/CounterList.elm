31f0e70077a2131399bb35eb26e0f6ddc7cded91
Matt Walker <walkermatt@longwayaround.org.uk>
2015-11-29 19:24:29 +0000

    Remove the need to manually manage counter IDs in ex4
    Includes several versions of a function to remove a value in a List at a
    given index

--- examples/4/CounterList.elm
+++ examples/4/CounterList.elm
@@ -8,19 +8,13 @@ import Html.Events exposing (..)
 
 -- MODEL
 
-type alias Model =
-    { counters : List ( ID, Counter.Model )
-    , nextID : ID
-    }
+type alias Model = { counters : List Counter.Model }
 
 type alias ID = Int
 
 
 init : Model
-init =
-    { counters = []
-    , nextID = 0
-    }
+init = { counters = [] }
 
 
 -- UPDATE
@@ -31,28 +25,48 @@ type Action
     | Modify ID Counter.Action
 
 
+-- removeAtIndex idx l =
+--   -- Create list of (index, value) tuples
+--   List.indexedMap (,) l |>
+--   -- Filter on index (using patten matching to pick apart the tuple)
+--   List.filter (\(i, _) -> i /= idx) |>
+--   -- Transform the list of tuples to a list of values (remove the index part)
+--   List.map (\(_, val) -> val)
+
+
+-- removeAtIndex idx l =
+--   -- Create list of (index, value) tuples
+--     List.indexedMap (,) l |>
+--     -- Use foldl to perform the filter and accumalate a list of values that
+--     -- should be retained
+--     List.foldl (\(k, v) l -> if k == idx then l else l ++ [v]) []
+
+
+removeAtIndex idx l =
+  List.take idx l ++ List.drop (idx + 1) l
+
+
 update : Action -> Model -> Model
 update action model =
   case action of
     Insert ->
       { model |
-          counters = ( model.nextID, Counter.init 0 ) :: model.counters,
-          nextID = model.nextID + 1
+          counters = Counter.init 0 :: model.counters
       }
 
     Remove id ->
       { model |
-          counters = List.filter (\(counterID, _) -> counterID /= id) model.counters
+          counters = removeAtIndex id model.counters
       }
 
     Modify id counterAction ->
-      let updateCounter (counterID, counterModel) =
+      let updateCounter counterID counterModel =
               if counterID == id then
-                  (counterID, Counter.update counterAction counterModel)
+                  Counter.update counterAction counterModel
               else
-                (counterID, counterModel)
+                counterModel
       in
-          { model | counters = List.map updateCounter model.counters }
+          { model | counters = List.indexedMap updateCounter model.counters }
 
 
 -- VIEW
@@ -61,11 +75,11 @@ view : Signal.Address Action -> Model -> Html
 view address model =
   let insert = button [ onClick address Insert ] [ text "Add" ]
   in
-      div [] (insert :: List.map (viewCounter address) model.counters)
+      div [] (insert :: List.indexedMap (viewCounter address) model.counters)
 
 
-viewCounter : Signal.Address Action -> (ID, Counter.Model) -> Html
-viewCounter address (id, model) =
+viewCounter : Signal.Address Action -> ID -> Counter.Model -> Html
+viewCounter address id model =
   let context =
         Counter.Context
           (Signal.forwardTo address (Modify id))
